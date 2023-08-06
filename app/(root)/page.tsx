66d9549e7c30a5106d30ec28aed16a26da1fd7ac
Adeniyi-Bella <adeniyibella@gmail.com>
2023-08-06 23:16:03 +0200

    basic authentication with @clerk

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+//app/page.tsx
+import { UserButton } from "@clerk/nextjs";
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/"/>
+    </div>
+  )
+}
