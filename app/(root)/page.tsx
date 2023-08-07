59cf04d15b4cdc60e09dbff4734cfc25efb7dbc3
Adeniyi-Bella <adeniyibella@gmail.com>
2023-08-07 23:00:27 +0200

    navbar and sidebars added to UI

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,8 +4,8 @@ import React from "react";
 
 export default function Home() {
   return (
-    <div>
-      <UserButton afterSignOutUrl="/"/>
-    </div>
-  )
+    <>
+          <h1 className='head-text text-left'>Home</h1>
+    </>
+  );
 }
